4fcd411877309c526ebe1fa8e7d199f70d2e79b1
Henry <henrybueno1998@hotmail.com>
2020-05-10 01:26:15 +0200

    Added more comments

--- assignment_1/assignment.py
+++ assignment_1/assignment.py
@@ -14,17 +14,21 @@ class RatingsBreakdown(MRJob):
             MRStep( reducer=self.reducer_sort_output) # we reduce the result sorted and print it
         ]
 
-    # a function
+    # a function to map the u.data input received, the result should be: [key: movieID, value: 1]
     def mapper_get_movies(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t') # we separate the rows by tabs  in between each other
-        yield movieID, 1 # we pass the movieId as the key, and it will receive initially 1 as a value
-
+        yield movieID, 1 # we select and pass the movieId as the key, and it will receive 1 as a value
+    
+    # a function to reduce the result that count the instances in which a movieID received a rating
     def reducer_count_ratings(self, movieID, values):
-        yield str(sum(values)).zfill(AMOUNT_OF_DECIMALS_FOR_RATINGS), movieID # turn the value into a string and fill the numbers with 0s to the left so it can be sorted, then pass the movieId, sum the values that share the movieId
-
+        yield str(sum(values)).zfill(AMOUNT_OF_LEADING_ZEROS_FOR_RATINGS), movieID # sum the values that share the movieId turn the value into a string 
+                                                                              # and fill the numbers with 0s to the left so it can be sorted, 
+                                                                              # then pass the movieId 
+    
+    # a function to print as a last step the movie id with the count of ratings sorted
     def reducer_sort_output(self, ratingsCount, movies):
         for movie in movies: # loop through the movieIds received in the previous reducer
-            yield 'Movie ' + movie, ratingsCount + ' ratings.' # this code will 'print' each movie with the count of ratings ascendingly
+            yield 'Movie ' + movie, ratingsCount + ' ratings.' # output each movie with the count of ratings ascendingly
 
 if __name__ == '__main__':
     RatingsBreakdown.run() # run the class above
